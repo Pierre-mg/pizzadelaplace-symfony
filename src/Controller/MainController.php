7e0450c04e62f742689115327b8bb08e832eccc1
Pierre-mg <pierrem.garcia@gmail.com>
2021-01-04 23:39:32 +0100

    home front ok

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Repository\AllergenProductRepository;
+use App\Repository\DessertPizzaRepository;
+use App\Repository\DrinkRepository;
+use App\Repository\InformationRepository;
 use App\Repository\RedPizzaRepository;
 use App\Repository\SpecialRedRepository;
 use App\Repository\SpecialWhiteRepository;
 use App\Repository\WhitePizzaRepository;
+use App\Repository\WineRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,12 +20,18 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(RedPizzaRepository $redPizza, WhitePizzaRepository $whitePizza, SpecialRedRepository $specialRed, SpecialWhiteRepository $specialWhite): Response
+    public function index(RedPizzaRepository $redPizza, WhitePizzaRepository $whitePizza, SpecialRedRepository $specialRed, SpecialWhiteRepository $specialWhite,
+    DessertPizzaRepository $dessertPizza, DrinkRepository $drink, WineRepository $wine, AllergenProductRepository $allergenProduct, InformationRepository $information): Response
     {
         return $this->render('main/index.html.twig', [
             'redPizza' => $redPizza->findAll(),
             'whitePizza' => $whitePizza->findAll(),
-            'specialRed' => $specialRed,
-            'specialWhite' => $specialWhite]);
+            'specialRed' => $specialRed->findAll(),
+            'specialWhite' => $specialWhite->findAll(),
+            'dessertPizza' => $dessertPizza->findAll(),
+            'drinks' => $drink->findAll(),
+            'wines' => $wine->findAll(),
+            'allergenProduct' => $allergenProduct->findAll(),
+            'information' => $information->findAll()]);
     }
 }
