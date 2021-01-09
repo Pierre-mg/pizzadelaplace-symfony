ff468c7831b9c8f0c58a6d685f9f16bcff66d69b
Pierre-mg <pierrem.garcia@gmail.com>
2021-01-09 17:21:19 +0100

    crud section WIP

---
+++ src/Controller/Admin/SectionController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Section;
+use App\Form\SectionType;
+use App\Repository\SectionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/section")
+ */
+class SectionController extends AbstractController
+{
+    /**
+     * @Route("/", name="section_index", methods={"GET"})
+     */
+    public function index(SectionRepository $sectionRepository): Response
+    {
+        return $this->render('admin/section/index.html.twig', [
+            'sections' => $sectionRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id<\d+>}/edit", name="section_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Section $section = null, EntityManagerInterface $manager): Response
+    {
+        if (!$section) {
+            throw $this->createNotFoundException('Section inexistante.');
+        }
+
+        $form = $this->createForm(SectionType::class, $section, 
+            ['attr' => [
+                    'novalidate' => 'novalidate'
+                ]
+            ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $manager->flush();
+
+            $this->addFlash('success', 'Section modifiée avec succès');
+
+            return $this->redirectToRoute('section_index');
+        }
+
+        return $this->render('admin/section/edit.html.twig', [
+            'section' => $section,
+            'form' => $form->createView(),
+        ]);
+    }
+
+}
--- src/Entity/Section.php
+++ src/Entity/Section.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\SectionRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=SectionRepository::class)
@@ -19,6 +20,8 @@ class Section
 
     /**
      * @ORM\Column(type="string", length=255)
+     * 
+     * @Assert\NotBlank
      */
     private $title;
 
