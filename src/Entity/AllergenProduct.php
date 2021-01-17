343435a381e4bcfe865659ff40e2d1a1af635d00
Pierre-mg <pierrem.garcia@gmail.com>
2021-01-17 12:04:05 +0100

    WIP Doctrine events

--- src/Entity/AllergenProduct.php
+++ src/Entity/AllergenProduct.php
@@ -8,6 +8,8 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=AllergenProductRepository::class)
+ * 
+ * @ORM\HasLifecycleCallbacks()
  */
 class AllergenProduct
 {
@@ -25,6 +27,32 @@ class AllergenProduct
      */
     private $name;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updated_at;
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setCreatedAtVAlue()
+    {
+        $this->created_at = new \DateTime();
+    }
+
+    /**
+    * @ORM\PreUpdate
+    */
+    public function setUpdatedAtValue()
+    {
+        $this->updated_at = new \DateTime();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -38,6 +66,30 @@ class AllergenProduct
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
 
         return $this;
     }
